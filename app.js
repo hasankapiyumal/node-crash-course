36538226acf34cb374477257f837e68eda5c078a
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2024-11-16 23:02:46 +0530

    create index.ejs file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 //listen for requests
 app.listen(3000);
+//register view engine
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
 //   res.send("<p>home page</p>");
 res.sendFile('./views/index.html',{root:__dirname});
