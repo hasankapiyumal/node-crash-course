40b47be62459d19b9e578fd5bbd766e8937f2275
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2024-11-16 23:20:24 +0530

    creating all ejs

--- app.js
+++ app.js
@@ -6,20 +6,23 @@ app.listen(3000);
 //register view engine
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-//   res.send("<p>home page</p>");
-res.sendFile('./views/index.html',{root:__dirname});
+  //   res.send("<p>home page</p>");
+//   res.sendFile("./views/index.html", { root: __dirname });
+res.render("index");
 });
 app.get("/about", (req, res) => {
-    // res.send("<p>about page</p>");
-    res.sendFile('./views/about.html',{root:__dirname});
-  });
+  // res.send("<p>about page</p>");
+//   res.sendFile("./views/about.html", { root: __dirname });
+res.render("about");
+});
 
-  //redirects
-  app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
-  })
+//redirects
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
 
-  //404 page
-  app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});
-  });
+//404 page
+app.use((req, res) => {
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+res.status(404).render("404");
+});
