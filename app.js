bccbbc8391b0af85d944e56903aa7ec3b9342d38
Hasanka Piyumal <119643446+hasankapiyumal@users.noreply.github.com>
2024-11-17 17:33:36 +0530

    create custom middlewares

--- app.js
+++ app.js
@@ -5,17 +5,39 @@ const app = express();
 app.listen(3000);
 //register view engine
 app.set("view engine", "ejs");
+
+app.use((req,res,next)=>{
+  console.log(req.hostname);
+  console.log(req.path);
+  console.log(req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   //   res.send("<p>home page</p>");
 //   res.sendFile("./views/index.html", { root: __dirname });
-res.render("index");
+const blogs =[
+    {title:'Yoshi finds eggs',snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'},
+    {title:'Mario finds stars',snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'},
+    {title:'How to defeat bowser',snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'}
+];
+res.render("index",{title:'Home',blogs});
 });
+
+app.use((req,res,next)=>{
+  console.log('in the next middleware');
+  next();
+});
+
 app.get("/about", (req, res) => {
   // res.send("<p>about page</p>");
 //   res.sendFile("./views/about.html", { root: __dirname });
-res.render("about");
+res.render("about",{title:'About'});
 });
 
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'Create a new blog'});
+})
 //redirects
 // app.get("/about-us", (req, res) => {
 //   res.redirect("/about");
@@ -24,5 +46,5 @@ res.render("about");
 //404 page
 app.use((req, res) => {
 //   res.status(404).sendFile("./views/404.html", { root: __dirname });
-res.status(404).render("404");
+res.status(404).render("404",{title:'404'});
 });
